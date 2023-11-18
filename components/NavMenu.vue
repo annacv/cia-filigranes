24efb5a75ad93b3744dea757770aeb1142b5ad18
annacv <acondalv@gmail.com>
2023-11-18 21:11:17 +0100

    Basic i18n & routes config

---
+++ components/NavMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLinkLocale :to="('/')">{{ $t('nav.home') }}</NuxtLinkLocale>
+      </li>
+      <li>
+        <NuxtLink :to="localePath('/espectacles')">{{ $t('nav.shows') }}</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink :to="localePath('/tallers')">{{ $t('nav.workshops') }}</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink :to="localePath('/animacions')">{{ $t('nav.performances') }}</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink :to="localePath('/filipersones')">{{ $t('nav.about') }}</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink :to="localePath('/contacte')">{{ $t('nav.contact') }}</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+const localePath = useLocalePath();
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <div class="fili">
+      companyia
+      <span class="fili__cia">filigranes</span>
+    </div>
+    <div class="fili_i18n-test">
+      {{ $t("welcome") }}
+      això és la home
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+.fili {
+  background-color: var(--main-bg-color) !important;
+  &__cia {
+    color: red;
+  }
+  &__i18n-test {
+    font-weight: bold;
+  }
+}
+</style>
