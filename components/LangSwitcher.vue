c45b36dc81880eae79d3171e8d064d13c6a6fb1f
annacv <acondalv@gmail.com>
2024-09-08 19:39:11 +0200

    Change switcher, apply status styles

--- components/LangSwitcher.vue
+++ components/LangSwitcher.vue
@@ -1,23 +1,22 @@
 <template>
-  <form class="language-selector">
-    <select
-      id="selectLang"
-      :aria-label="$t('selectLang')"
-      class="language-selector__field"
-      @change="onLocaleChanged"
-      v-model="selected"
+  <div
+    class="flex gap-4" role="radiogroup"
+    :aria-label="$t('selectLang')"
+  >
+    <button
+      v-for="lang in (locales as LocaleObject[])"
+      :key="lang.code"
+      :value="lang.code"
+      :aria-label="lang.name"
+      :aria-checked="isSelected(lang.code)"
+      :class="isSelected(lang.code) ? 'opacity-100' : 'opacity-20 hover:opacity-75'"
+      class="flex flex-col justify-center items-center"
+      @click="changeLocale(lang.code)"
     >
-      <option
-        v-for="lang in (locales as LocaleObject[])"
-        :key="lang.code"
-        :value="lang.code"
-        :aria-label="lang.name"
-        :aria-selected="lang.code === selected"
-      >
-        {{ lang.code }}
-      </option>
-    </select>
-  </form>
+      <span class="text-white text-sm">{{ lang.code }}</span>
+      <span class="bg-white h-1 w-1 rounded-full mt-1" :class="isSelected(lang.code) ? 'opacity-100' : 'opacity-20 hover:opacity-100'"></span>
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -28,8 +27,10 @@ const switchLocalePath = useSwitchLocalePath();
 const router = useRouter();
 const selected = ref<string>(locale.value);
 
-function onLocaleChanged(event: Event) {
-  const target = event.target as HTMLInputElement
-  router.push({ path: switchLocalePath(target.value) })
+const isSelected = (code: string) => code === selected.value;
+
+function changeLocale(code: string) {
+  router.push({ path: switchLocalePath(code) })
+  selected.value = code;
 }
 </script>
