35487106c3491f9255b724394014a5159865520d
annacv <acondalv@gmail.com>
2024-10-11 10:01:41 +0200

    WIP header & HeroCover

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,22 @@
 <template>
-  <main>
-    <div class="fili">
-      companyia
-      <span class="fili__cia w-full">filigranes</span>
-    </div>
-    <div class="fili_i18n-test">
-      {{ $t("welcome") }}
-      això és la home
-    </div>
-  </main>
+  <div class="fili">
+    companyia
+    <span class="fili__cia w-full">filigranes</span>
+  </div>
+  <div class="fili_i18n-test">
+    {{ $t("welcome") }}
+    això és la home
+  </div>
 </template>
 
 <style lang="scss">
 .fili {
   background-color: var(--main-bg-color) !important;
+
   &__cia {
     color: red;
   }
+
   &__i18n-test {
     font-weight: bold;
   }
