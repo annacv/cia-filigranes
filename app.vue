7a66827d8daaa9178437e028f5bc77fc23f5b624
anna condal <acondalv@gmail.com>
2025-06-27 19:48:00 +0200

    feat: structure app, layout, main

---
+++ app.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const nuxtApp = useNuxtApp()
+const { t } = useI18n()
+
+const head = useLocaleHead({
+  addDirAttribute: true,
+  identifierAttribute: "id",
+  addSeoAttributes: true,
+})
+const htmlAttrs = computed(() => head.value.htmlAttrs!)
+
+nuxtApp.hook('page:finish', () => {
+  window.scrollTo(0, 0)
+})
+</script>
+
+<template>
+  <Html :lang="htmlAttrs.lang" :dir="htmlAttrs.dir">
+  <Head>
+    <Title>{{ t('title') }}</Title>
+    <template v-for="link in head.link" :key="link.id">
+      <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang"/>
+    </template>
+    <template v-for="meta in head.meta" :key="meta.id">
+      <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
+    </template>
+  </Head>
+  <Body>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  </Body>
+  </Html>
+</template>
---
+++ components/MainContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <main role="main">
+    <div class="grid grid-cols-12 gap-4 pt-6 pb-6 min-w-full w-full min-h-screen h-full">
+      <div class="col-start-2 3xl:col-start-3 col-span-10 3xl:col-span-8">
+        <slot name="wrapped"></slot>
+      </div>
+      <slot name="full-grid"></slot>
+    </div>
+    <slot name="unwrapped"></slot>
+  </main>
+</template>
+<script setup lang="ts">
+</script>
