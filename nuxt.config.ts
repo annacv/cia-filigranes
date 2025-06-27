44355be975a6eb67c1aec180a3b9987a030d060c
anna condal <acondalv@gmail.com>
2025-06-27 19:07:27 +0200

    chore: wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,8 @@ import { routePages, localeObjects } from "./locales/index";
 
 export default defineNuxtConfig({
   app: {
-    head: {
-      meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "msapplication-TileColor", content: "#b91d47" },
-        { name: "theme-color", content: "#ffffff" },
-      ],
-      titleTemplate: "Cia.Filigranes",
-    },
     pageTransition: { name: "fade", mode: "out-in" },
-    rootId: "ciafiligranes",
+    rootId: "cia-filigranes",
   },
   modules: ["@nuxtjs/i18n", "@nuxtjs/tailwindcss", "@nuxtjs/device"],
   i18n: {
