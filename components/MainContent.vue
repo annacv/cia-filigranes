69da35f9d48a89a95427bd2ceccb53fc52b1dc0c
anna condal <acondalv@gmail.com>
2025-06-27 22:15:48 +0200

    feat: implement tailwind helper classes home

--- components/MainContent.vue
+++ components/MainContent.vue
@@ -1,7 +1,7 @@
 <template>
   <main role="main">
-    <div class="grid grid-cols-12 gap-4 pt-6 pb-6 min-w-full w-full min-h-screen h-full">
-      <div class="col-start-2 3xl:col-start-3 col-span-10 3xl:col-span-8">
+    <div class="grid-layout min-w-full w-full h-full">
+      <div class="layout-cols">
         <slot name="wrapped"></slot>
       </div>
       <slot name="full-grid"></slot>
@@ -9,5 +9,3 @@
     <slot name="unwrapped"></slot>
   </main>
 </template>
-<script setup lang="ts">
-</script>
