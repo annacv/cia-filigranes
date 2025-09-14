296f714fc05b4b7c9523dd44bc396c9f8b8e233c
anna condal <acondalv@gmail.com>
2025-09-14 19:49:31 +0200

    feat: enhance layout with new column classes and improve structure in MainContent and Synopsis components

--- components/Synopsis.vue
+++ components/Synopsis.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import { getImageUrl } from "~/composables/use-get-image-url.composable";
+import type { ImageRoute } from "~/types";
 
 const props = defineProps({
   alt: {
@@ -32,14 +33,15 @@ const props = defineProps({
     required: true,
     default: () => []
   },
+  title: {
+    type: String,
+    required: false
+  }
 })
 
 const { isMobile } = useDevice()
 const isHovered = ref(false)
 const imageUrl = getImageUrl(props.imageName, props.imageRoute);
-
-const rightStart = 'xl:col-start-2 lg:col-span-3 xl:col-span-3'
-const leftStart = 'xl:col-start-5 xl:col-span-12'
 
 const initialClipPath = 'polygon(100% 100%, 4% 100%, 20% 0, 100% 0)';
 const reversedClipPath = 'polygon(0 100%, 50% 100%, 100% 0, 0 0)';
@@ -58,40 +60,40 @@ const toggleHover = () => {
     @mouseenter="toggleHover"
     @mouseleave="toggleHover"
   >
-    <div
-      :class="[
-        'flex flex-col gap-4 col-start-1 col-span-6 p-5 lg:py-20 2xl:py-36',
-        !isReversed ? rightStart : leftStart
-      ]"
+    <div :class="[
+      'flex flex-col md:flex-row gap-0 xl:gap-5',
+      isReversed ? 'layout-cols--to-left md:flex-row-reverse' : 'layout-cols--to-right']"
     >
-      <p
-        class="text-base lg:text-xl"
-        :class="showFullContent ? 'flex flex-col gap-4' : 'line-clamp-5'">
-        <span
-          v-for="item in showFullContent ? synopsis : synopsis.slice(0, 1)"
-          :key="item.paragraph"
-        >
-          {{ item.paragraph }}
-        </span>
-      </p>
-
-      <slot name="extra-left-content"></slot>
-    </div>
-    <div
-      class="h-[400px] lg:h-auto bg-no-repeat bg-cover items-center shadow transition-all duration-1000 ease-[cubic-bezier(0.4,0,0.2,1)]"
-      :class="[
-        isHovered || isMobile ? 'bg-blend-soft-light' : 'bg-blend-hard-light',
-        'col-start-1 col-span-6',
-        !isReversed ? leftStart : rightStart
-      ]"
-      :style="{
-        backgroundImage: `linear-gradient(to right bottom, rgb(200 13 13 / 0.72), rgb(20 2 4 / 0.66)), url('${imageUrl}')`,
-        backgroundPosition: 'center center',
-        clipPath: !isMobile ? currentClipPath : 'none',
-      }"
-    >
-      <!-- Added img tag for Accessibility for screen readers -->
-      <img :src="imageUrl" :alt="alt" style="position: absolute; width: 1px; height: 1px; overflow: hidden; clip: rect(1px, 1px, 1px, 1px); white-space: nowrap;" aria-hidden="false" />
+      <div class="w-full lg:w-[50%] xl:w-[36%] flex flex-col gap-4 p-5 lg:py-20 2xl:py-36">
+        <h2 v-if="title" class="font-grotesk uppercase text-3xl lg:text-5xl">
+          {{ title }}
+        </h2>
+        <p
+          class="text-lg lg:text-xl"
+          :class="showFullContent ? 'flex flex-col gap-4' : 'line-clamp-5'">
+          <span
+            v-for="item in showFullContent ? synopsis : synopsis.slice(0, 1)"
+            :key="item.paragraph"
+          >
+            {{ item.paragraph }}
+          </span>
+        </p>
+        <slot name="extra-left-content"></slot>
+      </div>
+      <div
+        class="w-full h-[400px] md:h-auto bg-no-repeat bg-cover items-center shadow transition-all duration-1000 ease-[cubic-bezier(0.4,0,0.2,1)]"
+        :class="[
+          isHovered || isMobile ? 'bg-blend-soft-light' : 'bg-blend-hard-light'
+        ]"
+        :style="{
+          backgroundImage: `linear-gradient(to right bottom, rgb(200 13 13 / 0.72), rgb(20 2 4 / 0.66)), url('${imageUrl}')`,
+          backgroundPosition: 'center center',
+          clipPath: isMobile ? 'none' : currentClipPath
+        }"
+      >
+        <!-- Added img tag for Accessibility for screen readers -->
+        <img :src="imageUrl" :alt="alt" style="position: absolute; width: 1px; height: 1px; overflow: hidden; clip: rect(1px, 1px, 1px, 1px); white-space: nowrap;" aria-hidden="false" />
+      </div>
     </div>
   </div>
 </template>
