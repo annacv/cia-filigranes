f6a348baac30f28eff54d85204f6c5bc556ce50e
anna condal <acondalv@gmail.com>
2025-09-17 18:48:37 +0200

    feat: enhance Hero components with contentType support and scroll handling

--- components/HeroFooter.vue
+++ components/HeroFooter.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { getImageUrl } from "~/composables/use-get-image-url.composable";
-import type { ImageRoute } from "~/types";
-import {computed } from "vue";
+import { useGetColor } from "~/composables/use-get-color.composable";
+import { useScroll } from "~/composables/use-scroll.composable";
+import type { ImageRoute, ContentType } from "~/types";
+import { computed } from "vue";
 
 const props = defineProps({
   alt: {
@@ -15,11 +17,17 @@ const props = defineProps({
   imageRoute: {
     type: String as PropType<ImageRoute>,
     required: true
+  },
+  contentType: {
+    type: String as PropType<ContentType>,
+    default: 'shows'
   }
 });
 
 const { isMobile } = useDevice()
+const { hasReachedBottom } = useScroll()
 const imageUrl = getImageUrl(props.imageName, props.imageRoute);
+const { gradientOverlayValue } = useGetColor(props.contentType);
 
 const mobileHeight = '400px';
 const desktopHeight = '900px';
@@ -34,8 +42,9 @@ const deviceClipPath = computed(() => `polygon(0 0, 100% ${deviceClip.value}, 10
 <template>
   <div
     class="mt-24 w-full bg-no-repeat bg-cover bg-blend-hard-light shadow transition-all duration-500 ease-[cubic-bezier(0.4,0,0.2,1)]"
+    :class="hasReachedBottom ? 'bg-blend-soft-light' : 'bg-blend-hard-light'"
     :style="{
-      backgroundImage: `linear-gradient(to right bottom, rgb(200 13 13 / 0.72), rgb(20 2 4 / 0.66)), url('${imageUrl}')`,
+      backgroundImage: `linear-gradient(to right bottom, ${props.contentType ? gradientOverlayValue : 'var(--gradient-overlay-primary)'}), url('${imageUrl}')`,
       backgroundPosition: 'center center',
       clipPath: deviceClipPath,
       height: deviceHeight,
