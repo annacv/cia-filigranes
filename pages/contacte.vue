60ca1c9f6a6d6cc6e01f1be3eeccbeb68617a9c6
anna condal <acondalv@gmail.com>
2025-10-05 21:59:54 +0200

    Fix Google Maps iframe language parameters

--- pages/contacte.vue
+++ pages/contacte.vue
@@ -72,7 +72,7 @@ onBeforeUnmount(() => {
               
               <div class="flex flex-col items-start gap-2">
                 <iframe
-                  :src="`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5970.133862969208!2d2.0127018!3d41.56779600000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a492c3e2155299%3A0x4d072d8f6b4f6768!2sCia%20Filigranes!5e0!3m2!1sen!2ses!4v1759615126607!5m2!1sen!2ses&lang=${locale}`"
+                  :src="`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5970.133862969208!2d2.0127018!3d41.56779600000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a492c3e2155299%3A0x4d072d8f6b4f6768!2sCia%20Filigranes!5e0!3m2!1s${locale}!2s${locale}!4v1759615126607!5m2!1s${locale}!2s${locale}&lang=${locale}`"
                   class="w-full h-full aspect-video"
                   style="border:0;"
                   allowfullscreen
