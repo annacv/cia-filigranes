d1c9df24433fbf1c6d213876e25f41c2ab28d01d
anna condal <acondalv@gmail.com>
2025-10-09 22:48:11 +0200

    feat:404 page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,13 +1,6 @@
-<template>
-  <div>
-    <h1>Page not found</h1>
-    <p>Sorry, the page you're looking for doesn't exist.</p>
-    <NuxtLink to="/">Go to home page</NuxtLink>
-  </div>
-</template>
-
 <script setup>
-// This is a catch-all route that will match any route not defined in your pages
+import ArrowRight from "assets/icons/arrow-right.svg";
+// This is a catch-all route that will match any route not defined.
 // It will show a 404 page for any unknown routes
 
 definePageMeta({
@@ -17,4 +10,46 @@ definePageMeta({
     layout: 'error'
   }
 });
+
+const { t } = useI18n();
+const { isMobile } = useDevice();
+const imageSrc = useImageUrl('espectacles_circ-filixic', 'espectacles');
+const imageAlt = '404';
 </script>
+
+<template>
+  <div :class="[isMobile ? 'h-[72px]' : 'h-[87px]', 'bg-primary-500']"></div>
+  <MainContent class="bg-primary-500">
+    <template #wrapped>
+      <div class="flex flex-col items-center justify-center h-full bg-primary-500 pt-6 pb-32">
+        <img
+          class="h-[150px] md:h-[200px] aspect-square object-cover object-center pointer-events-none"
+          :src="imageSrc"
+          :alt="imageAlt"
+          style="clip-path: circle(50% at 50% 50%)"
+          loading="lazy"
+          draggable="false"
+        />
+        <h1 class="px-4 my-2 text-center text-white font-black uppercase text-6xl lg:text-7xl">
+          404
+        </h1>
+        <p class="font-black text-center !leading-tight text-lg md:text-xl text-white mb-5"
+        >{{ t('404.title') }}</p>
+        <p class="w-[280px] text-center !leading-tight text-lg md:text-xl text-white mb-5"
+        >{{ t('404.description') }}</p>
+        <FiliButton
+          href="/"
+          buttonClass="button-outline-primary"
+          target="_top"
+        >
+          <template #text>
+            {{ t('button.goHome') }}
+          </template>
+          <template #icon-left>
+            <ArrowRight class="arrow-right self-start mt-0.5 rotate-180"/>
+          </template>
+        </FiliButton>
+      </div>
+    </template>
+  </MainContent>
+</template>
