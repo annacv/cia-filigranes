6ddf528a6ebee0b66b925a50a572cdb45dd6b43a
anna condal <acondalv@gmail.com>
2025-10-09 20:56:53 +0200

    feat: enhance YT player for all devices

--- components/YoutubePlayer.vue
+++ components/YoutubePlayer.vue
@@ -6,6 +6,8 @@ interface PlayerVars {
   mute?: 0 | 1;
   start?: number;
   end?: number;
+  hl?: string;
+  playlist?: string;
   [key: string]: any;
 }
 
@@ -16,6 +18,8 @@ interface YouTubeProps {
   height?: number;
   trigger?: 'visible' | 'mouseover';
   aboveTheFold?: boolean;
+  webp?: boolean;
+  cookies?: boolean;
 }
 
 const props = withDefaults(defineProps<YouTubeProps>(), {
@@ -23,14 +27,24 @@ const props = withDefaults(defineProps<YouTubeProps>(), {
   height: 720,
   playerVars: () => ({
     autoplay: 0,
-  }),
-  image: () => ({
-    imageName: '',
-    imageRoute: '',
+    loop: 0,
+    mute: 0,
+    start: 0,
+    controls: 1,
+    playlist: 'TBbBS05njec,VOcQTqZ9C2A,rg3XzGQIPpU,dY4GelzNlRc,GQ67p38vqcY,lU5b5V7U6Wc,D46T_hy1Mck'
   }),
   trigger: 'visible',
   aboveTheFold: true,
+  webp: true,
+  cookies: false,
 })
+
+const { locale } = useI18n()
+
+const dynamicPlayerVars = computed(() => ({
+  ...props.playerVars,
+  hl: locale.value
+}))
 </script>
 
 <template>
@@ -38,8 +52,13 @@ const props = withDefaults(defineProps<YouTubeProps>(), {
     <ScriptYouTubePlayer
       :video-id="videoId"
       :player-options="{ host: 'https://www.youtube.com' }"
-      trigger="mouseover"
-      :above-the-fold="true"
+      :above-the-fold="aboveTheFold"
+      :trigger="trigger"
+      :cookies="cookies"
+      :webp="webp"
+      :player-vars="dynamicPlayerVars"
+      :width="width"
+      :height="height"
     > 
       <template #loading>
         <div class="bg-blue-500 text-white p-5">
