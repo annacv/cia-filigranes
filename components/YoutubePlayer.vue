a136719d6ad3283b77788b8516a48a6b4aa429d6
anna condal <acondalv@gmail.com>
2025-11-30 19:02:52 +0100

    refactor: wrap YouTube player in ClientOnly component for improved rendering

--- components/YoutubePlayer.vue
+++ components/YoutubePlayer.vue
@@ -52,40 +52,42 @@ const dynamicPlayerVars = computed(() => ({
 
 <template>
   <div class="relative w-full h-auto">
-    <Transition
-      enter-active-class="transition-all duration-500 ease-out"
-      enter-from-class="opacity-0 transform scale-95"
-      enter-to-class="opacity-100 transform scale-100"
-      leave-active-class="transition-all duration-300 ease-in"
-      leave-from-class="opacity-100 transform scale-100"
-      leave-to-class="opacity-0 transform scale-95"
-      mode="out-in"
-    >
-      <ScriptYouTubePlayer
-        v-if="canLoadYouTubeWithCookies"
-        key="player"
-        :video-id="videoId"
-        :player-options="{ host: 'https://www.youtube.com' }"
-        :above-the-fold="aboveTheFold"
-        :trigger="trigger"
-        :cookies="effectiveCookies"
-        :webp="webp"
-        :player-vars="dynamicPlayerVars"
-        :width="width"
-        :height="height"
-      > 
-        <template #loading>
-          <div class="bg-blue-500 text-white p-5">
-            Loading...
-          </div>
-        </template>
-      </ScriptYouTubePlayer>
-      <ConsentPlaceholder
-        v-else
-        key="placeholder"
-        :title="t('consent.youtube.title')"
-        :description="t('consent.youtube.consentRequired')"
-      />
-    </Transition>
+    <ClientOnly>
+      <Transition
+        enter-active-class="transition-all duration-500 ease-out"
+        enter-from-class="opacity-0 transform scale-95"
+        enter-to-class="opacity-100 transform scale-100"
+        leave-active-class="transition-all duration-300 ease-in"
+        leave-from-class="opacity-100 transform scale-100"
+        leave-to-class="opacity-0 transform scale-95"
+        mode="out-in"
+      >
+        <ScriptYouTubePlayer
+          v-if="canLoadYouTubeWithCookies"
+          key="player"
+          :video-id="videoId"
+          :player-options="{ host: 'https://www.youtube.com' }"
+          :above-the-fold="aboveTheFold"
+          :trigger="trigger"
+          :cookies="effectiveCookies"
+          :webp="webp"
+          :player-vars="dynamicPlayerVars"
+          :width="width"
+          :height="height"
+        > 
+          <template #loading>
+            <div class="bg-blue-500 text-white p-5">
+              Loading...
+            </div>
+          </template>
+        </ScriptYouTubePlayer>
+        <ConsentPlaceholder
+          v-else
+          key="placeholder"
+          :title="t('consent.youtube.title')"
+          :description="t('consent.youtube.consentRequired')"
+        />
+      </Transition>
+    </ClientOnly>
   </div>
 </template>
