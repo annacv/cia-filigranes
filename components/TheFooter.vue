4613c988670514b88f3156d7a100a4bebb6c44a7
anna condal <acondalv@gmail.com>
2026-04-08 13:06:50 +0200

    style: update footer layout and localization strings for consistency

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,7 +2,7 @@
 import { CONTACT } from "~/constants";
 
 const { t } = useI18n()
-const { isMobileOrTablet } = useResponsive()
+const { isMobile, isSmallTablet } = useResponsive()
 const { openModal } = useCookies()
 
 </script>
@@ -14,14 +14,14 @@ const { openModal } = useCookies()
     <div class="grid-layout bg-black text-white text-center text-sm">
       <div class="layout-cols flex flex-wrap flex-row items-center justify-center gap-2 xl:gap-4 py-8 md:py-12">
         <SocialMedia />
-        <b class="md:px-1">·</b>
+        <b class="md:px-1 leading-none">·</b>
         <a
           class="hover:opacity-80"
           :href="`mailto:${CONTACT.email}`"
         >
           {{ CONTACT.email }}
         </a>
-        <b class="md:px-1">·</b>
+        <b class="md:px-1 leading-none">·</b>
         <a
           class="hover:opacity-80"
           :href="CONTACT.adressHref">
@@ -30,13 +30,13 @@ const { openModal } = useCookies()
       </div>
     </div>
     <div class="grid-layout bg-neutral-900 py-8 md:py-6">
-      <div class="layout-cols text-white m-auto flex flex-col 2xl:flex-row lg:justify-between w-full items-center gap-1 gap-y-4">
+      <div class="layout-cols text-white m-auto flex flex-col xl:flex-row lg:justify-between w-full items-center gap-1 gap-y-4">
         <div class="flex flex-col lg:flex-row lg:flex-wrap justify-center items-center gap-1 gap-y-4 text-center text-xs">
           <p class="flex items-center gap-1 font-bold lg:text-xs text-sm">
             <CiaBrand class="text-white !w-8 md:!w-6 !h-auto pr-1" />
             Companyia Filigranes, 2026
           </p>
-          <b v-if="!isMobileOrTablet" class="px-2">·</b>
+          <b v-if="!isMobile || !isSmallTablet" class="px-2 leading-none">·</b>
           <span>
             {{ t('footer.design.text') }}
             <a
@@ -46,7 +46,7 @@ const { openModal } = useCookies()
               rel="noopener nofollow"
             >Anna Condal</a>
           </span>
-          <b v-if="!isMobileOrTablet" class="px-1">·</b>
+          <b v-if="!isMobile ||!isSmallTablet" class="px-1 leading-none">·</b>
           <span>
             {{ t('footer.foto.text') }}
             <a
@@ -55,14 +55,14 @@ const { openModal } = useCookies()
               target="_blank"
               rel="noopener nofollow"
             >Gerard Bañó</a>
-            <span class="px-1">|</span>
+            <span class="px-1 leading-none">|</span>
             <a
               class="font-bold transition-opacity duration-200 border-b-2 border-white pb-1 border-opacity-0 hover:border-opacity-50 hover:opacity-80"
               href="https://www.martacardellach.com/"
               target="_blank"
               rel="noopener nofollow"
             >Marta Garcia</a>
-            <span class="px-1">|</span>
+            <span class="px-1 leading-none">|</span>
             <a
               class="font-bold transition-opacity duration-200 border-b-2 border-white pb-1 border-opacity-0 hover:border-opacity-50 hover:opacity-80"
               href="https://www.evabalcellsfotografia.cat/"
@@ -71,20 +71,20 @@ const { openModal } = useCookies()
             >Eva Balcells</a>
           </span>
         </div>
-        <span v-if="isMobileOrTablet">·</span>
-        <p class="text-center text-xs">
+        <b v-if="isMobile || isSmallTablet" class="leading-none">·</b>
+        <p class="h-full flex flex-wrap justify-center items-center text-center text-xs">
           <NuxtLinkLocale to="/avis-legal" class="transition-opacity duration-200 border-b-2 border-white pb-1 border-opacity-0 hover:border-opacity-50 hover:opacity-80 cursor-pointer">
             {{ t('footer.legal.advice') }}
           </NuxtLinkLocale>
-          <b class="px-2">·</b>
+          <b class="px-2 leading-none">·</b>
           <NuxtLinkLocale to="/politica-cookies" class="transition-opacity duration-200 border-b-2 border-white pb-1 border-opacity-0 hover:border-opacity-50 hover:opacity-80 cursor-pointer">
             {{ t('footer.legal.cookies') }}
           </NuxtLinkLocale>
-          <b class="px-2">·</b>
+          <b class="px-2 leading-none">·</b>
           <NuxtLinkLocale to="/politica-privacitat" class="transition-opacity duration-200 border-b-2 border-white pb-1 border-opacity-0 hover:border-opacity-50 hover:opacity-80 cursor-pointer">
             {{ t('footer.legal.privacy') }}
           </NuxtLinkLocale>
-          <b class="px-2">·</b>
+          <b class="px-2 leading-none">·</b>
           <button
             type="button"
             class="transition-opacity duration-200 border-b-2 border-white pb-1 border-opacity-0 hover:border-opacity-50 hover:opacity-80 cursor-pointer"
